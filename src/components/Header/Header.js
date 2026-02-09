97e1bd991ce46d174fbb147ab03fee0c3d0b792d
wilku702 <willkung702@gmail.com>
2026-02-08 18:55:18 -0600

    Modified front page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
       <div className={`typewriter-animation ${skipAnimation ? 'skip-animation' : ''}`}>
         <h1>will kung</h1>
         <h1>full stack developer</h1>
-        <h1>react specialist, ai enthusiast</h1>
+        <p className="intro-text">Computer Science @ UT Austin. Backend specialist building scalable systems, shipping automation that doesn't break, and exploring AI reliability.</p>
       </div>
       <div className="scroll-down"></div>
     </div>
